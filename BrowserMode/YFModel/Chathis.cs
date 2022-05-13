3e35962e4bb29373a9e3a87b09ca5e5edd0449f2
LM0624 <58927205+LM0624@users.noreply.github.com>
2022-05-13 10:58:19 +0800

    Add files via upload

---
+++ BrowserMode/YFModel/Chathis.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YF.Model
+{
+    public class Chathis
+    {
+        private string _ID;
+        public string ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+
+        private string _UID1;
+        public string UID1
+        {
+            get { return _UID1; }
+            set { _UID1 = value; }
+        }
+
+        private string _UID2;
+        public string UID2
+        {
+            get { return _UID2; }
+            set { _UID2 = value; }
+        }
+
+        private int _isread;
+        public int Isread
+        {
+            get { return _isread; }
+            set { _isread = value; }
+        }
+
+        private string _sendTime;
+        public string SendTime
+        {
+            get { return _sendTime; }
+            set { _sendTime = value; }
+        }
+
+        private string _sendwords;
+        public string Sendwords
+        {
+            get { return _sendwords; }
+            set { _sendwords = value; }
+        }
+    }
+}
---
+++ BrowserMode/YFModel/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YF.Model
+{
+    public class User
+    {
+        private string _UID;
+        public string UID
+        {
+            get { return _UID; }
+            set { _UID = value; }
+        }
+        private string _account;
+        public string Account
+        {
+            get { return _account; }
+            set { _account = value; }
+        }
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+        private string _nickname;
+        public string Nickname
+        {
+            get { return _nickname; }
+            set { _nickname = value; }
+        }
+
+        private string _sign;
+        public string Sex
+        {
+            get { return _sign; }
+            set { _sign = value; }
+        }
+
+    }
+}
